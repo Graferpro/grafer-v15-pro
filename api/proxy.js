550e9dfddd3a5f72fafc6641623ca16c58f6b441
Graferpro <graferpol@gmail.com>
2026-02-04 14:48:28 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,12 +1,10 @@
-// api/proxy.js
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-    // CORS İzinleri (Her yerden erişim için)
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Vercel'deki gizli anahtarı al
+    // Vercel ayarlarından anahtarı al
     const API_KEY = process.env.API_KEY_FOREX;
 
     if (!API_KEY) {
@@ -14,16 +12,14 @@ export default async function handler(req, res) {
     }
 
     try {
-        // FastForex API'sine isteği biz yapıyoruz
-        const response = await fetch(`https://api.fastforex.io/fetch-all?api_key=${API_KEY}`);
-        
+        const url = `https://api.fastforex.io/fetch-all?api_key=${API_KEY}`;
+        const response = await fetch(url);
+
         if (!response.ok) {
             throw new Error(`Dış API Hatası: ${response.status}`);
         }
 
         const data = await response.json();
-        
-        // Veriyi frontend'e gönder
         res.status(200).json(data);
 
     } catch (error) {
