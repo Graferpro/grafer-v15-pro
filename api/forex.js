b174d40f1e4937e83b9d3769a86bf68d4b27a9b9
Graferpro <graferpol@gmail.com>
2026-02-07 11:03:28 +0100

    Update forex.js

--- api/forex.js
+++ api/forex.js
@@ -1,23 +1,18 @@
 export default async function handler(request, response) {
-// Vercel ayarlarından anahtarı çekiyoruz
-// Eğer Vercel'de ismini farklı koyduysan burayı değiştir (Örn: PROCESS.ENV.FAST_FOREX_KEY)
-const API_KEY = process.env.API_KEY_FOREX;
+    const API_KEY = process.env.API_KEY_FOREX;
 
-if (!API_KEY) {  
-    return response.status(500).json({ error: 'API Anahtarı (Environment Variable) bulunamadı!' });  
-}  
+    if (!API_KEY) {  
+        return response.status(500).json({ error: 'API Anahtarı bulunamadı!' });  
+    }  
 
-try {  
-    // Fast Forex'e istek atıyoruz  
-    const url = `https://api.fastforex.io/fetch-all?api_key=${API_KEY}`;  
-    const backendRes = await fetch(url);  
-    const data = await backendRes.json();  
+    try {  
+        const url = `https://api.fastforex.io/fetch-all?api_key=${API_KEY}`;  
+        const backendRes = await fetch(url);  
+        const data = await backendRes.json();  
 
-    // Veriyi senin siteye gönderiyoruz  
-    return response.status(200).json(data);  
+        return response.status(200).json(data);  
 
-} catch (error) {  
-    return response.status(500).json({ error: 'Veri çekilemedi', details: error.message });  
-}
-
+    } catch (error) {  
+        return response.status(500).json({ error: 'Forex verisi çekilemedi', details: error.message });  
+    }
 }
